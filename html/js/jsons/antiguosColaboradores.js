0a5c0a9574b0ae811c721258d9011e61c5778870
Al3xmm14 <molinaa757@gmail.com>
2023-12-11 16:07:44 -0600

    Falta index

---
+++ html/js/jsons/antiguosColaboradores.js
@@ -0,0 +1,51 @@
+// Ruta al archivo JSON
+var rutaJSON = 'JSON/antiguosColab.json';
+var rutaImg = 'images/participantes/';
+var num = 0;
+
+// Realizar solicitud (request) para obtener el JSON
+fetch(rutaJSON)
+    .then(response => response.json())
+    .then(jsonArray => {
+        // Obtener el contenedor
+        var informacionContainer = document.getElementById('informacion-container6');
+
+        // Iterar sobre cada elemento del arreglo
+        jsonArray.forEach(jsonInfo => {
+            num++;
+
+            // Crear elementos HTML para mostrar la información
+            var elementoInfo = document.createElement('div');
+            var res = rutaImg + jsonInfo.imagen;
+
+            elementoInfo.classList.add('info-container');
+            elementoInfo.innerHTML = `
+            <div class="card">
+                <div class="card-header" id="subseccion6-${num}">
+                    <button class="btn btn-link btn-linkcolor" type="button" data-toggle="collapse" data-target="#subcontenido6-${num}" aria-expanded="true">
+                        ${jsonInfo.nombre}
+                    </button>
+                </div>
+
+                <div id="subcontenido6-${num}" class="collapse" aria-labelledby="subseccion6-${num}" data-parent="#contenido6">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-12 col-lg-3 col-md-3 text-center">
+                                <img src="${res}" alt="Imagen de ${jsonInfo.nombre}" class="w-90">
+                            </div>
+                            <div class="col-12 col-lg-9 col-md-9 mb-3 text-justify">
+                                <p class="bloque">${jsonInfo.semblanza}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+  
+      `;
+
+            // Agregar el elemento al contenedor
+            informacionContainer.appendChild(elementoInfo);
+        });
+    })
+    .catch(error => console.error('Error al obtener el JSON:', error));
